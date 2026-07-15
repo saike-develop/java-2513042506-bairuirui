73344541db416a045c02900f0ead8f042e84df98
BobcGn <1483901658@qq.com>
2026-07-15 19:26:44 +0800

    缩进问题
    注意你的缩进，不要所有的东西都堆到最头，结构不清晰

--- src/basic/Exercise01.java
+++ src/basic/Exercise01.java
@@ -1,8 +1,7 @@
 package basic;
 
 public class Exercise01 {
-public static void main (String[]arges){
-    System.out.println("java世界，我来了");
-}
-
+    public static void main (String[]arges){
+        System.out.println("java世界，我来了");
+    }
 }
