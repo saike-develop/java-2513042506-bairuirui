898128d0a310fd2989b1bc43ab67243e8dbf2eb2
Ryan-Miller70 <2959769132@qq.com>
2026-07-21 21:22:58 +0800

    Exercise02

---
+++ src/basic/Exercise02.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        int age = 18;
+        double score = 95.5;
+        char ch = 'A';
+        boolean flag = true;
+        String name = "软件学院学生";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(score);
+    }
+}
