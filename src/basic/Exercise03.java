ba92d7000a6a0cf3ef4ca8c33e5f78fba9b53d80
Ryan-Miller70 <2959769132@qq.com>
2026-07-21 21:32:10 +0800

    Exercise03

---
+++ src/basic/Exercise03.java
@@ -0,0 +1,13 @@
+package basic;
+
+    public class Exercise03 {
+        public static void main(String[] args) {
+            int score = 88;
+            if(score >= 60){
+                System.out.println("考试及格");
+            }else{
+                System.out.println("考试不及格");
+            }
+        }
+
+}
